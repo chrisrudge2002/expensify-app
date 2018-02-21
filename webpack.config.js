be37de43b39555f136b475a34184de5f9b510b44
Chris Rudge <chrisrudge2002@gmail.com>
2018-02-21 09:02:26 +0000

    Configure webpack for produciton builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,46 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const publicDirPath = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.jsx',
-    output: {
-        path: publicDirPath,
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-      },
-    module: {
-        rules: [{ 
-            loader: 'babel-loader', 
-            test: /\.jsx?$/, 
-            exclude: /node_modules/ 
-        }, {
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-            test: /\.s?css$/,
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: publicDirPath,
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = (env === 'production');
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.jsx',
+        output: {
+            path: publicDirPath,
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: [".js", ".jsx"]
+          },
+        module: {
+            rules: [{ 
+                loader: 'babel-loader', 
+                test: /\.jsx?$/, 
+                exclude: /node_modules/ 
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true }
+                        }, {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [ CSSExtract ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: publicDirPath,
+            historyApiFallback: true
+        }
+    };
 };
